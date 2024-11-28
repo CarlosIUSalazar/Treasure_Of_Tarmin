ac133b2ed947e193b80f53c9e7dc2ed76a13d02f
Carlos Salazar <carlosiusalazar@gmail.com>
2024-11-28 19:23:15 +0900

    updated item positioning script

--- Assets/Scripts/ItemPositioning.cs
+++ Assets/Scripts/ItemPositioning.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class ItemPositioning : MonoBehaviour
 {
     public Transform player; // The player's transform
-    public float offsetDistance = 4.5f; // Distance from the center of the grid square to the edge
+    public float offsetDistance = 2.5f; // Distance from the center of the grid square to the edge
     public float gridSize = 10.0f; // Size of a grid square
 
     private Vector3 gridCenter; // The center of the item's grid square
@@ -25,7 +25,11 @@ public class ItemPositioning : MonoBehaviour
         }
 
         // Store the initial position as the grid center
-        gridCenter = transform.position;
+        gridCenter = new Vector3(
+        Mathf.Round(transform.position.x / gridSize) * gridSize,
+        transform.position.y,
+        Mathf.Round(transform.position.z / gridSize) * gridSize
+);
     }
 
     void Update()
@@ -57,19 +61,22 @@ public class ItemPositioning : MonoBehaviour
 
     void UpdateItemPosition()
     {
-        // Get the player's forward direction
         Vector3 playerForward = player.forward;
 
-        // Normalize the forward direction to get the primary axis (north, south, east, or west)
+        Vector3 offset;
         if (Mathf.Abs(playerForward.x) > Mathf.Abs(playerForward.z))
         {
             // Facing east or west
-            transform.position = gridCenter + new Vector3(Mathf.Sign(playerForward.x) * offsetDistance, 0, 0);
+            offset = new Vector3(Mathf.Sign(playerForward.x) * offsetDistance, 0, 0);
         }
         else
         {
             // Facing north or south
-            transform.position = gridCenter + new Vector3(0, 0, Mathf.Sign(playerForward.z) * offsetDistance);
+            offset = new Vector3(0, 0, Mathf.Sign(playerForward.z) * offsetDistance);
         }
+
+        Debug.Log($"GridCenter: {gridCenter}, Offset: {offset}, NewPosition: {gridCenter + offset}");
+
+        transform.position = gridCenter + offset;
     }
 }
