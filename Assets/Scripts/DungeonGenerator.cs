b19a264fb6fcb90e265ea50e9aa7a89ff84bfff7
Carlos Salazar <carlosiusalazar@gmail.com>
2025-02-15 00:21:27 +0900

    This covers all the mazes in Very Hard Mode

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class Multiple6BlockPatterns : MonoBehaviour
+public class AllPatternsDemo : MonoBehaviour
 {
     [Header("Block Variants (3 Colors × 3 Leg Types)")]
     // BLUE
@@ -25,57 +25,53 @@ public class Multiple6BlockPatterns : MonoBehaviour
     public GameObject MazeGreen2StairsTreasure;
 
     [Header("Spacing & Layout")]
-    public float verticalSpacing = 0.8f; 
+    public float verticalSpacing = 0.8f;
     public float generationDelay = 0.2f;
 
-    // We'll define bounding boxes for each floor in floorPositions[i].
-    // i=0 => floor1 => 1 slot
-    // i=1 => floor2 => 2 slots
-    // i=2 => floor3 => 5 slots
-    // i=3 => floor4 => 7 slots
-    // i=4 => floor5 => 9 slots
-    // i=5 => floor6 => 11 slots
+    // We'll define the bounding boxes for each floor in floorPositions[i].
+    // Floor1 => 1 slot
+    // Floor2 => 2 slots
+    // Floor3 => 5 slots
+    // Floor4 => 7 slots
+    // Floor5 => 9 slots
+    // Floor6 => 11 slots
     private List<(float xLeft, float xRight)>[] floorPositions;
 
-    // We'll store multiple patterns in a list
-    private List<bool[][]> sixBlockTopPatterns = new List<bool[][]>();
+    // We'll store all 8 patterns in a single list
+    private List<bool[][]> allPatterns = new List<bool[][]>();
 
     void Awake()
     {
-        // 1) define manual bounding boxes for each floor
+        // 1) Define bounding boxes for each floor
         DefineManualFloorPositions();
 
-        // 2) define the patterns
-        BuildPatterns();
+        // 2) Build all 8 patterns from your message
+        BuildAllPatterns();
     }
 
     void Start()
     {
-        // 3) pick a pattern (random or specific) and spawn it
-        // Example: pick random
-        int index = Random.Range(0, sixBlockTopPatterns.Count);
+        // 3) Pick one pattern at random
+        int index = Random.Range(0, allPatterns.Count);
+        bool[][] chosen = allPatterns[index];
 
-        // If you want to specifically test pattern #2, do: int index=1;
-        // or pattern #1 => index=0, etc.
-
-        bool[][] chosenPattern = sixBlockTopPatterns[index];
-        StartCoroutine(SpawnPattern(chosenPattern));
+        // 4) Spawn it from top down
+        StartCoroutine(SpawnPattern(chosen));
     }
 
     /// <summary>
-    /// Manually define x-centers for each floor:
-    /// floor1 => x=0
-    /// floor2 => x=-1, +1
+    /// We define bounding boxes for floors 1..6 as:
+    /// floor1 => x=0 => 1 slot
+    /// floor2 => x=-1,+1 => 2 slots
     /// floor3 => x=-2..+2 => 5 slots
     /// floor4 => x=-3..+3 => 7 slots
     /// floor5 => x=-4..+4 => 9 slots
     /// floor6 => x=-5..+5 => 11 slots
-    /// Each bounding box is 2 wide => [xCenter-1..xCenter+1].
+    /// Each slot is 2 wide => [xCenter-1..xCenter+1].
     /// </summary>
     void DefineManualFloorPositions()
     {
         floorPositions = new List<(float,float)>[6];
-
         floorPositions[0] = BuildFloorSlots(new float[]{ 0f });
         floorPositions[1] = BuildFloorSlots(new float[]{ -1f, +1f });
         floorPositions[2] = BuildFloorSlots(new float[]{ -2f, -1f, 0f, +1f, +2f });
@@ -96,53 +92,90 @@ public class Multiple6BlockPatterns : MonoBehaviour
     }
 
     /// <summary>
-    /// Build and store your multiple 6-block-top patterns in a list. 
-    /// Pattern #1 and Pattern #2 from your chat logs.
+    /// Builds all 8 patterns from your chat message. We store them in 'allPatterns'.
+    /// The user can rename them or reorder them as desired.
     /// </summary>
-    void BuildPatterns()
+    void BuildAllPatterns()
     {
-        // PATTERN #1 Skull 6 blocks
+        // Pattern1 => "Skull" (6-block top)
         bool[][] pattern1 = new bool[6][];
-
-        // floor1 => 1 slot
         pattern1[0] = new bool[1] { true };
-
-        // floor2 => 2 slots
         pattern1[1] = new bool[2] { true, true };
-
-        // floor3 => 5 slots => [true, false, false, false, true]
         pattern1[2] = new bool[5] { true, false, false, false, true };
-
-        // floor4 => 7 slots => [true, false, true, false, true, false, true]
         pattern1[3] = new bool[7] { true, false, true, false, true, false, true };
-
-        // floor5 => 9 slots => [true, false, false, false, true, false, false, false, true]
         pattern1[4] = new bool[9] { true, false, false, false, true, false, false, false, true };
+        pattern1[5] = new bool[11]{ true, false, true, false, true, false, true, false, true, false, true };
+        allPatterns.Add(pattern1);
 
-        // floor6 => 11 => [true, false, true, false, true, false, true, false, true, false, true]
-        pattern1[5] = new bool[11] { true, false, true, false, true, false, true, false, true, false, true };
-
-        sixBlockTopPatterns.Add(pattern1);
-
-        // PATTERN #2 Toilet 6 blocks
+        // Pattern2 => "Toilet" (6-block top)
         bool[][] pattern2 = new bool[6][];
-
         pattern2[0] = new bool[1] { true };
         pattern2[1] = new bool[2] { true, true };
-
-        // floor3 => 5 => [true, false, false, false, true]
         pattern2[2] = new bool[5] { true, false, false, false, true };
-
-        // floor4 => 7 => [true, false, false, false, false, true, true]
         pattern2[3] = new bool[7] { true, false, false, false, true, false, true };
-
-        // floor5 => 9 => [true, false, true, false, false, true, false, false, true]
         pattern2[4] = new bool[9] { true, false, true, false, false, true, false, false, true };
+        pattern2[5] = new bool[11]{ true, false, true, false, true, false, true, false, true, false, true };
+        allPatterns.Add(pattern2);
 
-        // floor6 => 11 => [true, false, true, false, true, false, true, false, true, false, true]
-        pattern2[5] = new bool[11] { true, false, true, false, true, false, true, false, true, false, true };
+        // Pattern3 => "floating mickey" (5-block top)
+        bool[][] p3 = new bool[6][];
+        p3[0] = new bool[1] { true };
+        p3[1] = new bool[2] { true, true };
+        p3[2] = new bool[5] { true, false, false, false, true };
+        p3[3] = new bool[7] { true, false, true, false, true, false, true };
+        p3[4] = new bool[9] { true, false, false, true, false, true, false, false, true };
+        p3[5] = new bool[11]{ true, false, true, false, true, false, false, true, false, false, true };
+        allPatterns.Add(p3);
 
-        sixBlockTopPatterns.Add(pattern2);
+        // Pattern4 => "pong paddle" (5-block top)
+        bool[][] p4 = new bool[6][];
+        p4[0] = new bool[1] { true };
+        p4[1] = new bool[2] { true, true };
+        p4[2] = new bool[5] { true, false, false, true, false };
+        p4[3] = new bool[7] { true, false, false, true, false, true, false };
+        p4[4] = new bool[9] { true, false, false, true, false, true, false, true, false };
+        p4[5] = new bool[11]{ true, false, true, false, true, false, false, true, false, true, false };
+        allPatterns.Add(p4);
+
+        // Pattern5 => "peace and love" (5-block top)
+        bool[][] p5 = new bool[6][];
+        p5[0] = new bool[1] { true };
+        p5[1] = new bool[2] { true, true };
+        p5[2] = new bool[5] { true, false, false, false, true };
+        p5[3] = new bool[7] { true, false, true, false, true, false, true };
+        p5[4] = new bool[9] { true, false, true, false, true, false, true, false, true };
+        p5[5] = new bool[11]{ true, false, false, true, false, false, true, false, true, false, true };
+        allPatterns.Add(p5);
+
+        // Pattern6 => "the bow" (5-block top)
+        bool[][] p6 = new bool[6][];
+        p6[0] = new bool[1] { true };
+        p6[1] = new bool[2] { true, true };
+        p6[2] = new bool[5] { false, true, false, false, true };
+        p6[3] = new bool[7] { false, true, false, true, false, false, true };
+        p6[4] = new bool[9] { false, true, false, true, false, false, true, false, true };
+        p6[5] = new bool[11]{ false, true, false, true, false, false, true, false, true, false, true };
+        allPatterns.Add(p6);
+
+        // Pattern7 => "cat paw" (5-block top)
+        bool[][] p7 = new bool[6][];
+        p7[0] = new bool[1] { true };
+        p7[1] = new bool[2] { true, true };
+        p7[2] = new bool[5] { true, false, false, false, true };
+        p7[3] = new bool[7] { true, false, true, false, true, false, true };
+        p7[4] = new bool[9] { true, false, true, false, false, true, false, false, true };
+        p7[5] = new bool[11]{ false, true, false, false, true, false, true, false, true, false, true };
+        allPatterns.Add(p7);
+
+        // Pattern8 => "olimpic rings" (5-block top)
+        bool[][] p8 = new bool[6][];
+        p8[0] = new bool[1] { true };
+        p8[1] = new bool[2] { true, true };
+        p8[2] = new bool[5] { true, false, true, false, true };
+        p8[3] = new bool[7] { true, false, false, true, false, false, true };
+        p8[4] = new bool[9] { true, false, false, true, false, true, false, false, true };
+        p8[5] = new bool[11]{ true, false, true, false, true, false, false, true, false, true, false };
+        allPatterns.Add(p8);
     }
 
     IEnumerator SpawnPattern(bool[][] pattern)
@@ -158,7 +191,7 @@ public class Multiple6BlockPatterns : MonoBehaviour
                 // bounding box
                 (float L, float R) = floorPositions[i][s];
                 float xCenter = 0.5f*(L+R);
-                float y = i*verticalSpacing;
+                float y = i * verticalSpacing;
                 Vector3 pos = new Vector3(xCenter, y, 0f);
 
                 if (i==0)
@@ -168,7 +201,7 @@ public class Multiple6BlockPatterns : MonoBehaviour
                 }
                 else
                 {
-                    // normal block => color + leg variant
+                    // normal block => color+legs
                     GameObject prefab = PickBlockVariant(i, s, pattern);
                     Instantiate(prefab, pos, Quaternion.identity, transform);
                 }
@@ -180,6 +213,7 @@ public class Multiple6BlockPatterns : MonoBehaviour
 
     GameObject PickTreasurePrefab()
     {
+        // random Tan or Green
         if (Random.Range(0,2)==0)
             return MazeTan2StairsTreasure;
         else
@@ -200,11 +234,11 @@ public class Multiple6BlockPatterns : MonoBehaviour
                 if (!below[s]) continue;
                 (float bL, float bR) = floorPositions[floorIndex-1][s];
                 if (DoOverlap(L, mid, bL, bR)) leftLeg=true;
-                if (DoOverlap(mid,R, bL, bR)) rightLeg=true;
+                if (DoOverlap(mid, R, bL, bR)) rightLeg=true;
             }
         }
 
-        // random color
+        // random color among [blue, green, tan]
         int colorIndex = Random.Range(0,3);
 
         if (leftLeg && rightLeg)
@@ -236,7 +270,7 @@ public class Multiple6BlockPatterns : MonoBehaviour
         }
         else
         {
-            // no overlap => default 2
+            // no overlap => default 2 legs
             switch(colorIndex)
             {
                 case 0: return MazeBlue2Stairs;
